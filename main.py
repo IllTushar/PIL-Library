5b653e2331398ac8c06533eede752589dadc992c
IllTushar <gtushar7668@gmail.com>
2023-12-06 23:30:49 +0530

    6.12.22x2

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-'''
-Image processing library=>
-OpenCv, PIL , PILLOW
-'''
 from PIL import Image as img
 
-file_name = r'C:\Users\gtush\OneDrive\Desktop\GUI\pil\icons8-dice-32.png'
-
-try:
-    images = img.open(file_name)
-    images.show()
-except Exception as e:
-    print(f"An error occurred: {e}")
+images = img.new("RGB", (200, 200), "red")
+images.show()

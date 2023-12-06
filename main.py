76ff5deb5479e185fc120fbd90378f70b718928c
IllTushar <gtushar7668@gmail.com>
2023-12-07 00:39:54 +0530

    7.12.23x5

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from PIL import Image as img
 
 file_name = r'C:\Users\gtush\OneDrive\Desktop\GUI\pil\please give me  1.png'
 images = img.open(file_name)
-images = images.rotate(angle=60, expand=True, fillcolor="white", center=(100, 100))
+images.show()
+
+images = images.resize((int(images.width / 2), int(images.height / 2)), resample=img.LANCZOS, box=(20, 20, 100, 100))
 images.show()

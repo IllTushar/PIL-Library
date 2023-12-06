2576e21635cfc26cf6583f7c8ee85d9d138f32ed
IllTushar <gtushar7668@gmail.com>
2023-12-06 23:57:31 +0530

    6.12.23x3

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from PIL import Image as img
 
-file_name = ''
+file_name = r'C:\Users\gtush\OneDrive\Desktop\GUI\pil\please give me  1.png'
+images = img.open(file_name)
+images = images.rotate(angle=60, expand=True, fillcolor="white", center=(100, 100))
+images.show()
